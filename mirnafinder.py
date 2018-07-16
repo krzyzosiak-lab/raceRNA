8c77c00d00721d89d8e219ae002a9a4c7a13ec23
Krzyzosiak lab <31852251+krzyzosiak-lab@users.noreply.github.com>
2018-07-16 16:44:17 +0100

    Create mirnafinder.py

---
+++ mirnafinder.py
@@ -0,0 +1,50 @@
+#raceRNA
+f1=open((input('File name with mature miRNA sequences(FASTA format):')),'r')
+while True:
+    repeat=str(input('Examined repeat (3-6 nt long):'))
+    if len(repeat)>6 or len(repeat)<3:
+        print('Sorry, examined repeat size need to be between 3 and 6 nt.')
+        continue
+    else:
+        break
+file_out=str(input('Name of results file:'))
+
+rev=repeat[::-1] #reversing repeat and subsequent steps to make reverse complement
+rev=rev.replace('A','V')
+rev=rev.replace('T','A')
+rev=rev.replace('U','A')
+rev=rev.replace('V','U')
+rev=rev.replace('G','D')
+rev=rev.replace('C','G')
+rev=rev.replace('D','C')
+
+a=f1.readlines()
+f1.close()
+
+if len(rev)==3:
+    rev=rev+rev
+elif len(rev)==4:
+    rev=rev+rev[0:2]
+else:
+    rev=rev+rev[0:1]
+
+word=rev*2
+unit_length=len(rev)
+d={}
+for i in range(0,unit_length): #frame dictionary generator
+    d["frame{0}".format(i)]=word[i:-(unit_length-i)]
+
+ile=1
+for i in a:
+    if ile%2==1: #sequence header
+        ii=i.split('|')
+        name=ii[0].strip('\n')
+    else: #sequence row
+        for j in list(d.values()): #extracting all possible variants of the SSR motif
+            seed=i[1:8] #extracting seed sequence (nucleotides poistion 2-8)
+            if j in seed:
+                f2=open(file_out,'a')
+                f2.write(name+'\n'+i)
+                f2.close()
+                break
+    ile+=1
